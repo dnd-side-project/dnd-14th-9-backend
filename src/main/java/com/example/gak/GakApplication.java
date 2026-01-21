f9c0fb62a128b5e6c17045d6e9db9c161787e97b
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-01-21 19:20:23 +0900

    Initial commit

---
+++ src/main/java/com/example/gak/GakApplication.java
@@ -0,0 +1,13 @@
+package com.example.gak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gak/GakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
