78bbf616942072febd589f6fe818c3fbacb459b6
yonghyeonpark <llyyoo93@gmail.com>
2026-02-03 01:35:03 +0900

    feat: 구글 OIDC 로그인 연동 및 회원 동기화 구현

---
+++ src/main/java/com/example/gak/global/security/oauth2/CustomOidcUserService.java
@@ -0,0 +1,40 @@
+package com.example.gak.global.security.oauth2;
+
+import java.util.Map;
+
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import com.example.gak.domain.member.service.MemberService;
+import com.example.gak.global.apiPayload.code.GeneralErrorCode;
+import com.example.gak.global.apiPayload.exception.GeneralException;
+import com.example.gak.global.security.oauth2.dto.GoogleMemberDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOidcUserService extends OidcUserService {
+
+	private final MemberService memberService;
+
+	@Override
+	public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+
+		Map<String, Object> attributes = oAuth2User.getAttributes();
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+		if (registrationId.equals("google")) {
+			GoogleMemberDto googleMemberResponse = new GoogleMemberDto(registrationId, attributes);
+			Long memberId = memberService.synchronize(googleMemberResponse);
+			return new CustomOidcUser(registrationId, memberId);
+		}
+
+		throw new GeneralException(GeneralErrorCode.UNSUPPORTED_PROVIDER);
+	}
+}
