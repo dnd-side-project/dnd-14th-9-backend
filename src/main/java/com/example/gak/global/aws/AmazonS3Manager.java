75c9521ec9b1794f30957f4bef39c37e04631a88
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-05 03:30:23 +0900

    feat: S3 업로드/삭제 및 키 생성 기능 구현(#24)

---
+++ src/main/java/com/example/gak/global/aws/AmazonS3Manager.java
@@ -0,0 +1,59 @@
+package com.example.gak.global.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.gak.global.configuration.AmazonConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AmazonS3Manager {
+
+    private final AmazonS3 amazonS3;
+    private final AmazonConfig amazonConfig;
+
+    public String uploadFile(String keyName, MultipartFile file) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        try {
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName, file.getInputStream(), metadata));
+        } catch (IOException e) {
+            log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
+            throw new RuntimeException("S3 업로드 오류 발생", e);
+        }
+        return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    public void deleteFile(String fileUrl) {
+        try {
+            String keyName = URI.create(fileUrl).getPath().substring(1);
+            amazonS3.deleteObject(amazonConfig.getBucket(), keyName);
+            log.info("Deleted S3 file: {}", keyName);
+
+        } catch (Exception e) {
+            log.error("S3 delete failed for file: {}", fileUrl, e);
+            throw new RuntimeException("S3 삭제 오류 발생", e);
+        }
+    }
+
+    public String generateProfileKeyName() {
+        String uuid = UUID.randomUUID().toString();
+        return amazonConfig.getProfilePath() + '/' + uuid;
+    }
+
+    public String generateSessionThumbnailKeyName() {
+        String uuid = UUID.randomUUID().toString();
+        return amazonConfig.getSessionThumbnailPath() + '/' + uuid;
+    }
+}
