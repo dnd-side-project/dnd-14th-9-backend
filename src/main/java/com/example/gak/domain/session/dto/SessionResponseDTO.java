21d467d8c817844eb1a6f168ba458214c157afa7
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-11 02:22:31 +0900

    feat: 세션 목록 조회 API 구현

---
+++ src/main/java/com/example/gak/domain/session/dto/SessionResponseDTO.java
@@ -0,0 +1,37 @@
+package com.example.gak.domain.session.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class SessionResponseDTO {
+
+    @Builder
+    @Getter
+    public static class SessionCardResponseDTO{
+        private String category;
+        private String title;
+        private String hostNickname;
+        private String status;
+        private int currentParticipants;
+        private int maxParticipants;
+        private int sessionDurationMinutes;
+        private LocalDateTime startTime;
+        private String imageUrl;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class sessionCardResponseListDTO {
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+        List<SessionCardResponseDTO> sessions;
+    }
+}
---
+++ src/main/java/com/example/gak/domain/session/service/SessionQueryService.java
@@ -0,0 +1,78 @@
+package com.example.gak.domain.session.service;
+
+import com.example.gak.domain.common.entity.enums.SessionCategory;
+import com.example.gak.domain.session.converter.SessionConverter;
+import com.example.gak.domain.session.dto.SessionResponseDTO;
+import com.example.gak.domain.session.entity.SessionRoom;
+import com.example.gak.domain.session.enums.DurationRange;
+import com.example.gak.domain.session.enums.SessionSort;
+import com.example.gak.domain.session.enums.TimeSlot;
+import com.example.gak.domain.session.repository.SessionRoomRepository;
+import com.example.gak.domain.session.repository.SessionSpecification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SessionQueryService {
+
+    private final SessionRoomRepository sessionRoomRepository;
+
+    public SessionResponseDTO.sessionCardResponseListDTO getSessions(
+            String keyword,
+            SessionCategory category,
+            SessionSort sort,
+            LocalDate startDate,
+            LocalDate endDate,
+            List<TimeSlot> timeSlots,
+            DurationRange durationRange,
+            Integer participants,
+            Integer requiredFocusRate,
+            Integer requiredAchievementRate,
+            int page,
+            int size
+    ){
+        Sort sortOption = createSortOption(sort.toString());
+        PageRequest pageRequest = PageRequest.of(page-1, size, sortOption);
+
+        Page<SessionRoom> sessionRooms = sessionRoomRepository.findAll(
+                SessionSpecification.sessionRoomFilter(
+                        keyword,
+                        category,
+                        startDate,
+                        endDate,
+                        timeSlots,
+                        durationRange,
+                        participants,
+                        requiredFocusRate,
+                        requiredAchievementRate
+                ),
+                pageRequest
+        );
+
+        List<SessionResponseDTO.SessionCardResponseDTO> searchResult =
+                sessionRooms.getContent().stream()
+                        .map(SessionConverter::toSessionCardResponseDTO)
+                        .toList();
+
+        SessionResponseDTO.sessionCardResponseListDTO result =
+                SessionConverter.toSessionCardResponseListDTO(searchResult,sessionRooms);
+
+        return result;
+    }
+
+    private Sort createSortOption(String sort) {
+        return switch (sort) {
+            case "POPULAR" -> Sort.by(Sort.Direction.DESC, "viewCount");
+            case "LATEST" -> Sort.by(Sort.Direction.DESC, "createdAt");
+            case "DEADLINE_APPROACHING" -> Sort.by(Sort.Direction.ASC, "startTime");
+            default -> Sort.by(Sort.Direction.DESC, "createdAt");
+        };
+    }
+}
