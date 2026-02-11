c4cbe8475d6f19e0df57a3fb6400ac16901d7162
yonghyeonpark <llyyoo93@gmail.com>
2026-02-11 16:55:19 +0900

    refactor: 회원 프로필 조회용 응답 DTO 추가

--- src/main/java/com/example/gak/domain/member/dto/MemberResponseDTO.java
+++ src/main/java/com/example/gak/domain/member/dto/MemberResponseDTO.java
@@ -5,16 +5,18 @@ import com.example.gak.domain.common.entity.enums.SessionCategory;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
-@Getter
 public class MemberResponseDTO {
 
-	private Long id;
-	private String nickname;
-	private String profileImageUrl;
-	private String bio;
-	private SessionCategory firstInterestCategory;
-	private SessionCategory secondInterestCategory;
-	private SessionCategory thirdInterestCategory;
-	private boolean firstLogin;
+	@Builder
+	@Getter
+	public static class GetMemberResponseDTO {
+		private Long id;
+		private String nickname;
+		private String profileImageUrl;
+		private String bio;
+		private SessionCategory firstInterestCategory;
+		private SessionCategory secondInterestCategory;
+		private SessionCategory thirdInterestCategory;
+		private boolean firstLogin;
+	}
 }
