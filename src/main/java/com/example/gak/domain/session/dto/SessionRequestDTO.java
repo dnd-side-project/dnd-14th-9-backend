c5db6b70fbd9d1462ab57054468c117a301010da
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-12 03:38:24 +0900

    feat: 세션 생성 기능 구현

---
+++ src/main/java/com/example/gak/domain/session/dto/SessionRequestDTO.java
@@ -0,0 +1,54 @@
+package com.example.gak.domain.session.dto;
+
+import com.example.gak.domain.common.entity.enums.SessionCategory;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+
+import jakarta.validation.constraints.*;
+import java.time.LocalDateTime;
+
+public class SessionRequestDTO {
+
+    @Getter
+    @Builder
+    public static class CreateSessionRequestDTO{
+        @NotBlank
+        @Size(max = 20)
+        private String title;
+
+        @NotBlank
+        @Size(max = 50)
+        private String summary;
+
+        @NotBlank
+        @Size(max = 100)
+        private String notice;
+
+        @NotNull
+        private SessionCategory category;
+
+        @NotNull
+        @Future
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        private LocalDateTime startTime;
+
+        @NotNull
+        @Positive
+        private Integer sessionDurationMinutes;
+
+        @NotNull
+        @Positive
+        private Integer maxParticipants;
+
+        @Min(0)
+        private Integer requiredFocusRate = 0;
+
+        @Min(0)
+        private Integer requiredAchievementRate = 0;
+    }
+}
---
+++ src/main/java/com/example/gak/domain/session/service/SessionCommendService.java
@@ -0,0 +1,64 @@
+package com.example.gak.domain.session.service;
+
+import com.example.gak.domain.member.entity.Member;
+import com.example.gak.domain.member.repository.MemberRepository;
+import com.example.gak.domain.session.dto.SessionRequestDTO;
+import com.example.gak.domain.session.entity.SessionRoom;
+import com.example.gak.domain.session.entity.enums.SessionRoomStatus;
+import com.example.gak.domain.session.repository.SessionRoomRepository;
+import com.example.gak.global.apiPayload.code.GeneralErrorCode;
+import com.example.gak.global.apiPayload.exception.GeneralException;
+import com.example.gak.global.aws.AmazonS3Manager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class SessionCommendService {
+
+    private static final long MIN_START_MINUTES = 5;
+
+    private final MemberRepository memberRepository;
+    private final SessionRoomRepository sessionRoomRepository;
+    private final AmazonS3Manager amazonS3Manager;
+
+    public SessionRoom createSession(
+            SessionRequestDTO.CreateSessionRequestDTO request,
+            MultipartFile image,
+            Long memberId
+    ){
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(GeneralErrorCode.MEMBER_NOT_FOUND));
+
+        LocalDateTime minAllowedStartTime  = LocalDateTime.now().plusMinutes(MIN_START_MINUTES);
+        if (request.getStartTime().isBefore(minAllowedStartTime )) {
+            throw new GeneralException(GeneralErrorCode.SESSION_START_TIME_TOO_SOON);
+        }
+
+        String imageUrl;
+        if(!image.isEmpty()) {
+            String keyName = amazonS3Manager.generateSessionThumbnailKeyName();
+            imageUrl = amazonS3Manager.uploadFile(keyName, image);
+        }else{
+            imageUrl = ""; // 기본 이미지 디자인 완성 시 URL 추가
+        }
+
+        SessionRoom newSessionRoom =  new SessionRoom(
+                request.getCategory(),
+                request.getTitle(),
+                request.getSummary(),
+                request.getNotice(),
+                imageUrl,
+                request.getStartTime(),
+                request.getSessionDurationMinutes(),
+                request.getMaxParticipants(),
+                SessionRoomStatus.WAITING,
+                member
+        );
+        sessionRoomRepository.save(newSessionRoom);
+        return newSessionRoom;
+    }
+}
