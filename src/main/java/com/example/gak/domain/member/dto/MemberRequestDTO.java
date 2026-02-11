3316c339faf3e2f34f55c0c750458c7868cbaf8d
yonghyeonpark <llyyoo93@gmail.com>
2026-02-11 18:46:37 +0900

    fix: DTO 역직렬화를 위한 생성자 어노테이션 추가

--- src/main/java/com/example/gak/domain/member/dto/MemberRequestDTO.java
+++ src/main/java/com/example/gak/domain/member/dto/MemberRequestDTO.java
@@ -2,11 +2,15 @@ package com.example.gak.domain.member.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class MemberRequestDTO {
 
+	@AllArgsConstructor
+	@NoArgsConstructor
 	@Builder
 	@Getter
 	public static class UpdateMemberNicknameRequestDTO {
