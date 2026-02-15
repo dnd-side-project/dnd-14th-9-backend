f15fc50389b644b4a41d20a666b40411f705b21c
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-15 16:27:03 +0900

    refactor: SessionRoomMemberRepository에서 미사용 메서드 제거

--- src/main/java/com/example/gak/domain/session/repository/SessionRoomMemberRepository.java
+++ src/main/java/com/example/gak/domain/session/repository/SessionRoomMemberRepository.java
@@ -2,22 +2,10 @@ package com.example.gak.domain.session.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.gak.domain.session.entity.SessionRoomMember;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface SessionRoomMemberRepository extends JpaRepository<SessionRoomMember, Long> {
 
     void deleteByMemberId(Long memberId);
 
-    int countBySessionRoomId(Long sessionRoomId);
-
     boolean existsBySessionRoomIdAndMemberId(Long sessionId, Long memberId);
-
-    @Query("""
-                select count(srm) > 0
-                from SessionRoomMember srm
-                where srm.sessionRoom.id = :sessionId
-                  and srm.role = 'HOST'
-            """)
-    boolean existsSessionHost(@Param("sessionId") Long sessionId);
 }
