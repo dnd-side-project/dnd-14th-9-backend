a2fc50eef34c5b061269c6cf4f34ce0335a3f6ff
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-17 03:37:18 +0900

    feat: todo 완료상태 토글 기능 구현

---
+++ src/main/java/com/example/gak/domain/task/service/TaskCommandService.java
@@ -0,0 +1,40 @@
+package com.example.gak.domain.task.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.gak.domain.session.entity.enums.SessionRoomStatus;
+import com.example.gak.domain.task.entity.SubTask;
+import com.example.gak.domain.task.repository.SubTaskRepository;
+import com.example.gak.global.apiPayload.code.GeneralErrorCode;
+import com.example.gak.global.apiPayload.exception.GeneralException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TaskCommandService {
+
+	private final SubTaskRepository subTaskRepository;
+
+	public void toggleSubTaskCompletion(Long subTaskId, Long memberId) {
+
+		SubTask subTask = subTaskRepository.findById(subTaskId)
+			.orElseThrow(() -> new GeneralException(GeneralErrorCode.SUBTASK_NOT_FOUND));
+
+		if (subTask.getTask().getSessionRoom().getStatus() != SessionRoomStatus.IN_PROGRESS) {
+			throw new GeneralException(GeneralErrorCode.SUBTASK_COMPLETION_NOT_ALLOWED);
+		}
+
+		if (!subTask.getTask().getMember().getId().equals(memberId)) {
+			throw new GeneralException(GeneralErrorCode.SUBTASK_ACCESS_DENIED);
+		}
+
+		if (subTask.getTask().getSessionRoom().getStatus() != SessionRoomStatus.IN_PROGRESS) {
+			throw new GeneralException(GeneralErrorCode.SUBTASK_COMPLETION_NOT_ALLOWED);
+		}
+
+		subTask.toggleCompletion();
+	}
+}
