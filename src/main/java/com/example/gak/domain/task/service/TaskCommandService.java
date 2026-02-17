8f05fb19c8ab85e6c272b9f7344a860348c3c78f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-18 04:18:42 +0900

    feat: 세션 TODO 수정 기능 구현

--- src/main/java/com/example/gak/domain/task/service/TaskCommandService.java
+++ src/main/java/com/example/gak/domain/task/service/TaskCommandService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.gak.domain.session.entity.enums.SessionRoomStatus;
+import com.example.gak.domain.task.dto.SubTaskRequestDTO;
 import com.example.gak.domain.task.entity.SubTask;
 import com.example.gak.domain.task.repository.SubTaskRepository;
 import com.example.gak.global.apiPayload.code.GeneralErrorCode;
@@ -32,5 +33,24 @@ public class TaskCommandService {
 		}
 
 		subTask.toggleCompletion();
+	}
+
+	public void updateSubtask(
+		Long subtaskId,
+		Long memberId,
+		SubTaskRequestDTO.UpdateSubTaskDTO request
+	) {
+		SubTask subTask = subTaskRepository.findById(subtaskId)
+			.orElseThrow(() -> new GeneralException(GeneralErrorCode.SUBTASK_NOT_FOUND));
+
+		if (!subTask.getTask().getMember().getId().equals(memberId)) {
+			throw new GeneralException(GeneralErrorCode.SUBTASK_ACCESS_DENIED);
+		}
+
+		if (subTask.getTask().getSessionRoom().getStatus() != SessionRoomStatus.WAITING) {
+			throw new GeneralException(GeneralErrorCode.SUBTASK_COMPLETION_NOT_ALLOWED);
+		}
+
+		subTask.updateSubTaskTitle(request.getTodoContent());
 	}
 }
