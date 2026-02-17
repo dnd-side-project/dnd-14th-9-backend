2d6224175f98913685eb20948e914684cf56cd00
yonghyeonpark <llyyoo93@gmail.com>
2026-02-17 21:02:28 +0900

    feat: 회원 이메일 수정 요청 DTO 구현

--- src/main/java/com/example/gak/domain/member/dto/MemberRequestDTO.java
+++ src/main/java/com/example/gak/domain/member/dto/MemberRequestDTO.java
@@ -3,6 +3,7 @@ package com.example.gak.domain.member.dto;
 import com.example.gak.domain.common.entity.enums.SessionCategory;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -27,6 +28,18 @@ public class MemberRequestDTO {
 		@NotBlank
 		@Size(min = 2, max = 10)
 		private String nickname;
+	}
+
+	@Schema(name = "회원 이메일 수정 요청")
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	@Getter
+	public static class UpdateMemberEmailRequestDTO {
+
+		@Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true)
+		@Email
+		private String email;
 	}
 
 	@Schema(name = "회원 관심카테고리 수정 요청")
