b4f31522051c1d884ea7fa1b6290a8687dc94d21
yonghyeonpark <llyyoo93@gmail.com>
2026-02-19 01:51:40 +0900

    chore: CORS 설정에 도메인 추가

--- src/main/java/com/example/gak/global/security/SecurityConfig.java
+++ src/main/java/com/example/gak/global/security/SecurityConfig.java
@@ -56,7 +56,11 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("https://localhost:3000", "http://localhost:3000"));
+		configuration.setAllowedOrigins(List.of(
+			"https://localhost:3000",
+			"http://localhost:3000",
+			"https://api.gak.today"
+		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setExposedHeaders(List.of("Authorization"));
