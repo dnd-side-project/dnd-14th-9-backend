bed1b808d8569cf53105e0c74034a8f934c7432f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-19 06:34:18 +0900

    chore: RedisMessageListenerContainer 설정 추가

---
+++ src/main/java/com/example/gak/global/redis/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.gak.global.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(
+		RedisConnectionFactory connectionFactory,
+		RedisSubscriber redisSubscriber
+	) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(redisSubscriber, new PatternTopic("waiting/*"));
+
+		return container;
+	}
+}
