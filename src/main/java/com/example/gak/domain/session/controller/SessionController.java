b48ee5e812e6e02cd9c0128ed0558d6c0fde7a5f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-19 16:58:40 +0900

    feat: 세션 진행 중 참여자 상태 토글 API 구현

--- src/main/java/com/example/gak/domain/session/controller/SessionController.java
+++ src/main/java/com/example/gak/domain/session/controller/SessionController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -133,5 +134,15 @@ public class SessionController {
 		@PathVariable Long sessionId
 	) {
 		return sessionQueryService.getCurrentWaitingRoom(sessionId);
+	}
+
+	@Operation(summary = "세션 진행 중 참여자 상태 (집중/자리비움) 토글")
+	@PatchMapping("/{sessionId}/me/status")
+	public ApiResponse<SessionResponseDTO.ToggleSessionMemberStatusResponseDTO> toggleParticipantStatus(
+		@PathVariable Long sessionId,
+		@AuthenticationPrincipal CustomOAuth2User oAuth2User
+	) {
+		return ApiResponse.onSuccess(
+			sessionCommandService.toggleSessionRoomMemberStatus(sessionId, oAuth2User.getMemberId()));
 	}
 }
