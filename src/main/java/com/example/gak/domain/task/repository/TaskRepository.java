00aba0ba7aa83ebf5d26b6c1f527e1d2bf82193d
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-22 04:42:32 +0900

    remove: 사용하지 않는 TaskRepository 메서드 삭제

--- src/main/java/com/example/gak/domain/task/repository/TaskRepository.java
+++ src/main/java/com/example/gak/domain/task/repository/TaskRepository.java
@@ -4,9 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.example.gak.domain.task.entity.Task;
 
@@ -17,11 +14,4 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 	Optional<Task> findBySessionRoomIdAndMemberId(Long sessionRoomId, Long memberId);
 
 	List<Task> findAllBySessionRoomIdAndMemberIdIn(Long sessionRoomId, List<Long> memberIds);
-
-	@Modifying
-	@Query("""
-		DELETE FROM Task t 
-		WHERE t IN :tasks
-		""")
-	void deleteAllInBatch(@Param("tasks") List<Task> tasks);
 }
