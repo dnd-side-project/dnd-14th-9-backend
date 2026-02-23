a1b712348449000eaf33ac5fb910d71018239ddf
yonghyeonpark <llyyoo93@gmail.com>
2026-02-23 17:49:36 +0900

    feat: 회원 리포트 통계 정보 조회 응답 DTO 추가

--- src/main/java/com/example/gak/domain/member/dto/MemberResponseDTO.java
+++ src/main/java/com/example/gak/domain/member/dto/MemberResponseDTO.java
@@ -1,5 +1,8 @@
 package com.example.gak.domain.member.dto;
 
+import java.util.List;
+
+import com.example.gak.domain.common.entity.enums.EmojiType;
 import com.example.gak.domain.common.entity.enums.SessionCategory;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -114,5 +117,56 @@ public class MemberResponseDTO {
 
 		@Schema(nullable = true)
 		private SessionCategory thirdInterestCategory;
+	}
+
+	@Schema(name = "회원 리포트 통계 정보 응답")
+	@Builder
+	@Getter
+	public static class GetReportStats {
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private long focusedTime;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private long totalParticipationTime;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private int todoCompletionRate;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private int focusRate;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private List<SessionParticipationStat> sessionParticipationStats;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private List<ReceivedEmojiStat> receivedEmojis;
+	}
+
+	@Schema(name = "세션 참여 현황 응답")
+	@Builder
+	@Getter
+	public static class SessionParticipationStat {
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private SessionCategory categoryName;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private int count;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private int rate;
+	}
+
+	@Schema(name = "받은 이모지 현황 응답")
+	@Builder
+	@Getter
+	public static class ReceivedEmojiStat {
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private EmojiType emojiName;
+
+		@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+		private int count;
 	}
 }
