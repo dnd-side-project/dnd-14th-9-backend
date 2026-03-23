3dbfafa8ab8e4f10f3e7785a30b1bba9d38ab521
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-03-23 13:34:23 +0900

    feat: 세션 수정 API 구현

--- src/main/java/com/example/gak/domain/session/controller/SessionController.java
+++ src/main/java/com/example/gak/domain/session/controller/SessionController.java
@@ -215,4 +215,17 @@ public class SessionController {
 		sessionCommandService.sessionDelete(sessionId, memberId);
 		return ApiResponse.onSuccess(null);
 	}
+
+	@Operation(summary = "세션 수정 API")
+	@PatchMapping(path = "/{sessionId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ApiResponse<Void> patchSession(
+		@PathVariable Long sessionId,
+		@AuthenticationPrincipal CustomOAuth2User oAuth2User,
+		@RequestPart("request") @Valid SessionRequestDTO.PatchSessionRequestDTO request,
+		@RequestPart(value = "image", required = false) MultipartFile image
+	) {
+		Long memberId = oAuth2User.getMemberId();
+		sessionCommandService.sessionPatch(sessionId, memberId, request, image);
+		return ApiResponse.onSuccess(null);
+	}
 }
