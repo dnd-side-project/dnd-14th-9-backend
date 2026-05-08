29803a276aa7683ab35736411545009a162eae55
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-05-09 02:52:27 +0900

    feat: 채팅 메시지 DB 저장 추가

--- src/main/java/com/example/gak/domain/chat/service/ChatCommandService.java
+++ src/main/java/com/example/gak/domain/chat/service/ChatCommandService.java
@@ -6,20 +6,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.gak.domain.chat.dto.ChatRequestDTO;
+import com.example.gak.domain.chat.entity.ChatMessage;
+import com.example.gak.domain.chat.repository.ChatMessageRepository;
+import com.example.gak.domain.member.entity.Member;
+import com.example.gak.domain.member.repository.MemberRepository;
+import com.example.gak.domain.session.entity.SessionRoom;
+import com.example.gak.domain.session.repository.SessionRoomRepository;
 import com.example.gak.global.redis.RedisPublisher;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ChatCommandService {
 
 	private final RedisPublisher redisPublisher;
+	private final ChatMessageRepository chatMessageRepository;
+	private final SessionRoomRepository sessionRoomRepository;
+	private final MemberRepository memberRepository;
 
 	public void sendMessage(Long sessionId, Long memberId, ChatRequestDTO.SendChatMessageRequestDTO dto) {
+		SessionRoom sessionRoom = sessionRoomRepository.getReferenceById(sessionId);
+		Member member = memberRepository.getReferenceById(memberId);
+
 		redisPublisher.chatMessagePublish(
 			sessionId,
 			toChatMessageResponseDTO(
@@ -27,5 +37,13 @@ public class ChatCommandService {
 				dto.getContent(),
 				dto.getType(),
 				dto.getQuickActionType()));
+
+		chatMessageRepository.save(new ChatMessage(
+			dto.getContent(),
+			dto.getType(),
+			dto.getQuickActionType(),
+			sessionRoom,
+			member
+		));
 	}
 }
